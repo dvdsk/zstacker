7226e39eb7ef82aabf1e89bfcb062acd0b316806
Guy Or <guydude1@gmail.com>
2023-11-05 08:56:33 +0200

    wip example

--- examples/ping.rs
+++ examples/ping.rs
@@ -1,6 +1,8 @@
 use std::time::Duration;
 
-use mt_interface::{data::MtCommand, wire::GeneralSerialPacket};
+use mt_interface::{
+    constants::{DeviceSpecificConfigurationItem, NetworkSpecificConfigurationItem}, data::MtCommand, wire::GeneralSerialPacket,
+};
 use serialport::SerialPort;
 
 fn main() {
@@ -18,13 +20,49 @@ fn main() {
         ("Ping", MtCommand::sys_ping(), true),
         ("Version", MtCommand::sys_version(), true),
         ("Extended Address", MtCommand::sys_get_extaddr(), true),
-        ("Start Timer", MtCommand::sys_osal_start_timer(3, 0xabcd), true),
+        (
+            "Start Timer",
+            MtCommand::sys_osal_start_timer(3, 0xabcd),
+            true,
+        ),
         ("Stop Timer", MtCommand::sys_osal_stop_timer(3), true),
         ("Random", MtCommand::sys_random(), true),
         ("Device Info", MtCommand::util_get_device_info(), true),
         ("NV Info", MtCommand::util_get_nv_info(), true),
         ("Time Alive", MtCommand::util_time_alive(), true),
         ("SRNG Gen", MtCommand::util_srng_gen(), true),
+        (
+            "Nv Read logical type",
+            MtCommand::sys_osal_nv_read(
+                DeviceSpecificConfigurationItem::ZCD_NV_LOGICAL_TYPE as u16,
+                0,
+            ),
+            true,
+        ),
+        (
+            "Nv Read direct db",
+            MtCommand::sys_osal_nv_read(
+                DeviceSpecificConfigurationItem::ZCD_NV_ZDO_DIRECT_CB as u16,
+                0,
+            ),
+            true,
+        ),
+        (
+            "Nv Read startup option",
+            MtCommand::sys_osal_nv_read(
+                DeviceSpecificConfigurationItem::ZCD_NV_STARTUP_OPTION as u16,
+                0,
+            ),
+            true,
+        ),
+        (
+            "Nv Read PAN-ID",
+            MtCommand::sys_osal_nv_read(
+                NetworkSpecificConfigurationItem::ZCD_NV_PANID as u16,
+                0,
+            ),
+            true,
+        ),
     ];
 
     for (desc, command, should_read) in commands {
@@ -32,7 +70,7 @@ fn main() {
     }
 }
 
-fn send_command(port: &mut Box<dyn SerialPort>, mt_cmd: MtCommand, should_read:bool, desc:&str) {
+fn send_command(port: &mut Box<dyn SerialPort>, mt_cmd: MtCommand, should_read: bool, desc: &str) {
     let packet = GeneralSerialPacket::from_cmd(mt_cmd);
     let tx = packet.to_frame();
     println!("####{}", desc);
@@ -46,7 +84,7 @@ fn send_command(port: &mut Box<dyn SerialPort>, mt_cmd: MtCommand, should_read:b
         let len = port
             .read(serial_buf.as_mut_slice())
             .expect("Found no data!");
-        
+
         print!("Read: ");
         print_packet(&serial_buf, len);
     }
